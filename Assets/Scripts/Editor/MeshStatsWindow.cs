1c6d9f841b31c05ccf943160b104e8f8c9d250a1
Silvian73 <yurykuz65@gmail.com>
2021-06-01 19:56:05 +0300

    Task 1 - Added Filtering

--- Assets/Scripts/Editor/MeshStatsWindow.cs
+++ Assets/Scripts/Editor/MeshStatsWindow.cs
@@ -9,12 +9,18 @@ namespace MegameTestTask.Editor
     public class MeshStatsWindow : EditorWindow
     {
         private const int NameFieldWidth = 150;
-        private const int VertexCountFieldWidth = 80;
+        private const int VerticesCountFieldWidth = 90;
         private const int PolygonCountFieldWidth = 90;
         private const int UsesInSceneFieldWidth = 85;
-        private const int VertexSumInSceneFieldWidth = 120;
+        private const int VerticesSumInSceneFieldWidth = 130;
         private const int ReadableFieldWidth = 60;
         private const int UVLightmapFieldWidth = 80;
+        private const int FilteringLabelWidth = 100;
+        private const int FilteringStringFieldWidth = 200;
+        private const int FilteringMinMaxLabelWidth = 30;
+        private const int FilteringIntFieldWidth = 50;
+        private const int ToggleWidth = 15;
+        private const int EnumPopupWidth = 100;
 
         private const int UpdateDataEveryXFrames = 10;
 
@@ -22,6 +28,24 @@ namespace MegameTestTask.Editor
 
         private List<SingleMeshStats> _meshStats;
         private Vector2 scrollPosition;
+
+        private bool filterByName;
+        private bool filterByVerticesCount;
+        private bool filterByPolygonsCount;
+        private bool filterByUsesInScene;
+        private bool filterByVerticesSumInScene;
+        
+        private string nameFilter = string.Empty;
+        private int minVerticesCount;
+        private int maxVerticesCount;
+        private int minPolygonsCount;
+        private int maxPolygonsCount;
+        private int minUsesInScene;
+        private int maxUsesInScene;
+        private int minVerticesSumInScene;
+        private int maxVerticesSumInScene;
+        private RequiredBool requiredReadable;
+        private RequiredBool requiredUVLightmap;
         
         [MenuItem("Window/Mesh Stats")]
         public static void Init()
@@ -41,63 +65,163 @@ namespace MegameTestTask.Editor
 
         private void OnGUI()
         {
-            if (Time.renderedFrameCount % UpdateDataEveryXFrames == 0)
-            {
-                UpdateData();
-            }
+            DrawTable();
+            DrawFiltering();
             
-            GUILayout.BeginHorizontal();
-                GUILayout.Label("Name", GUILayout.Width(NameFieldWidth));
-                GUILayout.Label("Vertex count", GUILayout.Width(VertexCountFieldWidth));
-                GUILayout.Label("Polygon count", GUILayout.Width(PolygonCountFieldWidth));
-                GUILayout.Label("Uses in scene", GUILayout.Width(UsesInSceneFieldWidth));
-                GUILayout.Label("Vertex sum in scene", GUILayout.Width(VertexSumInSceneFieldWidth));
-                GUILayout.Label("Readable", GUILayout.Width(ReadableFieldWidth));
-                GUILayout.Label("UV Lightmap", GUILayout.Width(UVLightmapFieldWidth));
-            GUILayout.EndHorizontal();
             
-            var meshStats = GetMeshStats();
-            scrollPosition = GUILayout.BeginScrollView(scrollPosition);
-            foreach (var singleMeshStats in meshStats)
-            {
+            void DrawTable()
+            { 
+                if (Time.renderedFrameCount % UpdateDataEveryXFrames == 0)
+                {
+                    UpdateData();
+                }
+                
                 GUILayout.BeginHorizontal();
-                    bool focus = GUILayout.Button(singleMeshStats.Name, labelLikeButtonStyle, GUILayout.Width(NameFieldWidth));
-                    if (focus)
-                    {
-                        Selection.activeGameObject = FindObjectsOfType<MeshFilter>()
-                            .First(x => x.sharedMesh == singleMeshStats.SharedMesh)
-                            .gameObject;
-                        SceneView.FrameLastActiveSceneView();
-                    }
-                    GUILayout.Label(singleMeshStats.VertexCount.ToString(), GUILayout.Width(VertexCountFieldWidth));
-                    GUILayout.Label(singleMeshStats.PolygonCount.ToString(), GUILayout.Width(PolygonCountFieldWidth));
-                    GUILayout.Label(singleMeshStats.UsesInScene.ToString(), GUILayout.Width(UsesInSceneFieldWidth));
-                    GUILayout.Label(singleMeshStats.VertexSumInScene.ToString(), GUILayout.Width(VertexSumInSceneFieldWidth));
-
-                    EditorGUI.BeginDisabledGroup(!singleMeshStats.HasImporter);
-                        bool isReadablePreviousValue = singleMeshStats.IsReadable;
-                        bool isReadableNewValue =
-                            GUILayout.Toggle(isReadablePreviousValue, string.Empty, GUILayout.Width(ReadableFieldWidth));
-                        if (isReadablePreviousValue != isReadableNewValue)
-                        {
-                            singleMeshStats.Importer.isReadable = isReadableNewValue;
-                            AssetDatabase.ImportAsset(singleMeshStats.Path, ImportAssetOptions.ForceUpdate);
-                            UpdateData();
-                        }
-
-                        bool uvLightmapPreviousValue = singleMeshStats.UVLightmap;
-                        bool uvLightmapNewValue =
-                            GUILayout.Toggle(uvLightmapPreviousValue, string.Empty, GUILayout.Width(UVLightmapFieldWidth));
-                        if (uvLightmapPreviousValue != uvLightmapNewValue)
-                        {
-                            singleMeshStats.Importer.generateSecondaryUV = uvLightmapNewValue;
-                            AssetDatabase.ImportAsset(singleMeshStats.Path, ImportAssetOptions.ForceUpdate);
-                            UpdateData();
-                        }
-                    EditorGUI.EndDisabledGroup();
+                    GUILayout.Label("Name", EditorStyles.boldLabel, GUILayout.Width(NameFieldWidth));
+                    GUILayout.Label("Vertices count", EditorStyles.boldLabel, GUILayout.Width(VerticesCountFieldWidth));
+                    GUILayout.Label("Polygons count", EditorStyles.boldLabel, GUILayout.Width(PolygonCountFieldWidth));
+                    GUILayout.Label("Uses in scene", EditorStyles.boldLabel, GUILayout.Width(UsesInSceneFieldWidth));
+                    GUILayout.Label("Vertices sum in scene", EditorStyles.boldLabel, GUILayout.Width(VerticesSumInSceneFieldWidth));
+                    GUILayout.Label("Readable", EditorStyles.boldLabel, GUILayout.Width(ReadableFieldWidth));
+                    GUILayout.Label("UV Lightmap", EditorStyles.boldLabel, GUILayout.Width(UVLightmapFieldWidth));
                 GUILayout.EndHorizontal();
+                
+                var meshStats = GetMeshStats();
+                FilterMeshStats();
+                scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+                foreach (var singleMeshStats in meshStats)
+                {
+                    GUILayout.BeginHorizontal();
+                        bool focus = GUILayout.Button(singleMeshStats.Name, labelLikeButtonStyle, GUILayout.Width(NameFieldWidth));
+                        if (focus)
+                        {
+                            Selection.activeGameObject = FindObjectsOfType<MeshFilter>()
+                                .First(x => x.sharedMesh == singleMeshStats.SharedMesh)
+                                .gameObject;
+                            SceneView.FrameLastActiveSceneView();
+                        }
+                        GUILayout.Label(singleMeshStats.VerticesCount.ToString(), GUILayout.Width(VerticesCountFieldWidth));
+                        GUILayout.Label(singleMeshStats.PolygonsCount.ToString(), GUILayout.Width(PolygonCountFieldWidth));
+                        GUILayout.Label(singleMeshStats.UsesInScene.ToString(), GUILayout.Width(UsesInSceneFieldWidth));
+                        GUILayout.Label(singleMeshStats.VerticesSumInScene.ToString(), GUILayout.Width(VerticesSumInSceneFieldWidth));
+
+                        EditorGUI.BeginDisabledGroup(!singleMeshStats.HasImporter);
+                            bool isReadablePreviousValue = singleMeshStats.IsReadable;
+                            bool isReadableNewValue =
+                                GUILayout.Toggle(isReadablePreviousValue, string.Empty, GUILayout.Width(ReadableFieldWidth));
+                            if (isReadablePreviousValue != isReadableNewValue)
+                            {
+                                singleMeshStats.Importer.isReadable = isReadableNewValue;
+                                AssetDatabase.ImportAsset(singleMeshStats.Path, ImportAssetOptions.ForceUpdate);
+                                UpdateData();
+                            }
+
+                            bool uvLightmapPreviousValue = singleMeshStats.UVLightmap;
+                            bool uvLightmapNewValue =
+                                GUILayout.Toggle(uvLightmapPreviousValue, string.Empty, GUILayout.Width(UVLightmapFieldWidth));
+                            if (uvLightmapPreviousValue != uvLightmapNewValue)
+                            {
+                                singleMeshStats.Importer.generateSecondaryUV = uvLightmapNewValue;
+                                AssetDatabase.ImportAsset(singleMeshStats.Path, ImportAssetOptions.ForceUpdate);
+                                UpdateData();
+                            }
+                        EditorGUI.EndDisabledGroup();
+                    GUILayout.EndHorizontal();
+                }
+                GUILayout.EndScrollView();
+
+
+                void FilterMeshStats()
+                {
+                    if (filterByName)
+                        meshStats = meshStats
+                            .Where(x => x.Name.ToLower().Contains(nameFilter.ToLower()))
+                            .ToList();
+                    if (filterByVerticesCount)
+                    {
+                        meshStats = meshStats
+                            .Where(x => x.VerticesCount >= minVerticesCount)
+                            .Where(x => x.VerticesCount <= maxVerticesCount)
+                            .ToList();
+                    }
+                    if (filterByPolygonsCount)
+                    {
+                        meshStats = meshStats
+                            .Where(x => x.PolygonsCount >= minPolygonsCount)
+                            .Where(x => x.PolygonsCount <= maxPolygonsCount)
+                            .ToList();
+                    }
+                    if (filterByUsesInScene)
+                    {
+                        meshStats = meshStats
+                            .Where(x => x.UsesInScene >= minUsesInScene)
+                            .Where(x => x.UsesInScene <= maxUsesInScene)
+                            .ToList();
+                    }
+                    if (filterByVerticesSumInScene)
+                    {
+                        meshStats = meshStats
+                            .Where(x => x.VerticesSumInScene >= minVerticesSumInScene)
+                            .Where(x => x.VerticesSumInScene <= maxVerticesSumInScene)
+                            .ToList();
+                    }
+                    if (requiredReadable != RequiredBool.Any)
+                    {
+                        bool requiredValue = requiredReadable == RequiredBool.True ? true : false;
+                        meshStats = meshStats
+                            .Where(x => x.IsReadable == requiredValue)
+                            .ToList();
+                    }
+                    if (requiredUVLightmap != RequiredBool.Any)
+                    {
+                        bool requiredValue = requiredUVLightmap == RequiredBool.True ? true : false;
+                        meshStats = meshStats
+                            .Where(x => x.UVLightmap == requiredValue)
+                            .ToList();
+                    }
+                }
             }
-            GUILayout.EndScrollView();
+
+            void DrawFiltering()
+            {
+                EditorGUILayout.LabelField("Filtering", EditorStyles.boldLabel);
+                EditorGUILayout.BeginHorizontal();
+                    filterByName = EditorGUILayout.Toggle(filterByName, GUILayout.Width(ToggleWidth));
+                    EditorGUILayout.LabelField("Name", GUILayout.Width(FilteringLabelWidth));
+                    EditorGUI.BeginDisabledGroup(!filterByName);
+                        nameFilter = EditorGUILayout.TextField(nameFilter, GUILayout.Width(FilteringStringFieldWidth));
+                    EditorGUI.EndDisabledGroup();
+                EditorGUILayout.EndHorizontal();
+                DrawMinMaxRange("Vertices count", ref minVerticesCount, ref maxVerticesCount, ref filterByVerticesCount);
+                DrawMinMaxRange("Polygons count", ref minPolygonsCount, ref maxPolygonsCount, ref filterByPolygonsCount);
+                DrawMinMaxRange("Uses in scene", ref minUsesInScene, ref maxUsesInScene, ref filterByUsesInScene);
+                DrawMinMaxRange("Vertices sum in scene", ref minVerticesSumInScene, ref maxVerticesSumInScene, ref filterByVerticesSumInScene);
+                DrawRequiredBoolLine("Readable", ref requiredReadable);
+                DrawRequiredBoolLine("UV lightmap", ref requiredUVLightmap);
+
+
+                void DrawMinMaxRange(string label, ref int min, ref int max, ref bool isFilteringActive)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                        isFilteringActive = EditorGUILayout.Toggle(isFilteringActive, GUILayout.Width(ToggleWidth));
+                        EditorGUILayout.LabelField(label, GUILayout.Width(FilteringLabelWidth));
+                        EditorGUI.BeginDisabledGroup(!isFilteringActive);
+                            EditorGUILayout.LabelField("Min", GUILayout.Width(FilteringMinMaxLabelWidth));
+                            min = EditorGUILayout.IntField(min, GUILayout.Width(FilteringIntFieldWidth));
+                            EditorGUILayout.LabelField("Max", GUILayout.Width(FilteringMinMaxLabelWidth));
+                            max = EditorGUILayout.IntField(max, GUILayout.Width(FilteringIntFieldWidth));
+                        EditorGUI.EndDisabledGroup();
+                    EditorGUILayout.EndHorizontal();
+                }
+
+                void DrawRequiredBoolLine(string label, ref RequiredBool requiredBool)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                        EditorGUILayout.LabelField(label, GUILayout.Width(FilteringLabelWidth));
+                        requiredBool = (RequiredBool)EditorGUILayout.EnumPopup(requiredBool, GUILayout.Width(EnumPopupWidth));
+                    EditorGUILayout.EndHorizontal();
+                }
+            }
         }
 
         public void UpdateData()
@@ -121,8 +245,8 @@ namespace MegameTestTask.Editor
                     var newMeshStats = new SingleMeshStats();
                     newMeshStats.SharedMesh = meshFilter.sharedMesh;
                     newMeshStats.Name = newMeshStats.SharedMesh.name;
-                    newMeshStats.VertexCount = newMeshStats.SharedMesh.vertexCount;
-                    newMeshStats.PolygonCount = newMeshStats.SharedMesh.triangles.Length / 3;
+                    newMeshStats.VerticesCount = newMeshStats.SharedMesh.vertexCount;
+                    newMeshStats.PolygonsCount = newMeshStats.SharedMesh.triangles.Length / 3;
                     newMeshStats.UsesInScene = 1;
                     newMeshStats.IsReadable = newMeshStats.SharedMesh.isReadable;
                     newMeshStats.Path = AssetDatabase.GetAssetPath(newMeshStats.SharedMesh);
@@ -143,8 +267,8 @@ namespace MegameTestTask.Editor
         {
             public Mesh SharedMesh;
             public string Name;
-            public int VertexCount;
-            public int PolygonCount;
+            public int VerticesCount;
+            public int PolygonsCount;
             public int UsesInScene;
             public bool IsReadable;
             public bool UVLightmap;
@@ -152,7 +276,14 @@ namespace MegameTestTask.Editor
             public string Path;
             public bool HasImporter;
             
-            public int VertexSumInScene => VertexCount * UsesInScene;
+            public int VerticesSumInScene => VerticesCount * UsesInScene;
+        }
+        
+        private enum RequiredBool
+        {
+            Any = 0,
+            True = 1,
+            False = 2
         }
     }
 }
